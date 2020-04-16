c9a1d3e21aa315fd02ce1e0702e6bd43c263bf1e
Jhonathan Vitor <jhonathanvitor@gmail.com>
2020-04-16 01:18:34 -0300

    First Commit

---
+++ src/components/Tabs/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {Container, TabsContainer, TabItem, TabText} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export default function Tabs() {
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#fff" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import React from 'react';
 import '~/config/ReactotronConfig';
 
 import Routes from '~/routes';
+import {StatusBar} from 'react-native';
 
-const App = () => <Routes />;
+const App = () => (
+  <>
+    <StatusBar barStule="light-content" backgroundColor="#353535" />
+    <Routes />
+  </>
+);
 
 export default App;
